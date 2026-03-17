def4187ee215b3bcd27102049851a6b1466a4e15
KOS-noise <lkos.noisel@gmail.com>
2026-03-17 10:52:58 +0900

    EmployeeDto 수정

--- src/main/java/com/employee/api/dto/EmployeeDto.java
+++ src/main/java/com/employee/api/dto/EmployeeDto.java
@@ -1,5 +1,9 @@
 package com.employee.api.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 @Getter
@@ -9,29 +13,20 @@ import lombok.*;
 @Builder
 public class EmployeeDto {
     private Long id;
+
+    @NotBlank(message = "직원 firstName은 필수 입력 항목입니다.")
     private String firstName;
+    @NotBlank(message = "직원 lastName은 필수 입력 항목입니다.")
     private String lastName;
+
+    @NotBlank(message = "직원 email은 필수 입력 항목입니다.")
+    @Email
     private String email;
+
+    @NotNull(message = "직원의 부서코드는 필수 입력 항목입니다.") // null 방지 (필수)
+    @Positive(message = "올바른 부서 형식이 아닙니다.") // 0 또는 음수 방지
     private Long departmentId;
 
     private DepartmentDto departmentDto;
 
-//    public EmployeeDto(Long id,String firstName,String lastName, String email) {
-//        this.id = id;
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//        this.email = email;
-//    }
-//
-//
-//    public EmployeeDto(Long id,String firstName,String lastName, String email, Long departmentId) {
-//        this(id,firstName,lastName,email);
-//        this.departmentId = departmentId;
-//    }
-//
-//
-//    public EmployeeDto(Long id,String firstName,String lastName, String email, DepartmentDto departmentDto) {
-//        this(id,firstName,lastName,email);
-//        this.departmentDto = departmentDto;
-//    }
 }
